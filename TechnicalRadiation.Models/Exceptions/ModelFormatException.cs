1ed1d62a4f48ddf6c4acc74f460e0fcfa27b4818
Kristofer <kristofergauti@gmail.com>
2021-09-20 18:52:53 +0000

    Remaining files

---
+++ TechnicalRadiation.Models/Exceptions/ModelFormatException.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class ModelFormatException : Exception {
+    public ModelFormatException() : base("Model is not properly formatted")
+    {
+    }
+
+    public ModelFormatException(string message)
+        : base(message)
+    {
+    }
+
+    public ModelFormatException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
